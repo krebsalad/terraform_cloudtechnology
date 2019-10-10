2637561263de046adbbad8d6bb648c11d8b03ccf
krebsalad <johanmgina7@gmail.com>
2019-10-10 14:46:47 +0200

    Update template_data.tf

--- modules/ubuntu_1604_template_data/template_data.tf
+++ modules/ubuntu_1604_template_data/template_data.tf
@@ -6,12 +6,12 @@ module "ubuntu-1604-data" {
   
   # set data
   pool_name = "ubuntu-pool"
-  pool_dir = "/tmp/terraform-libvirtpool-ubuntu"
+  pool_dir = "/tmp/terraform_libvirt_provider_images/"
   domain_name = "ubuntu-domain"
   image_name = "ubuntu-16.04-amd64.qcow2"
   common_name = "ubuntu-16.04"
   # image_source = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
-  image_source = "/home/terraform/downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  image_source = "/home/terraform/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
   user_data_source = "${path.module}/config/user_data.cfg"
   network_config_source = "${path.module}/config/network_config.cfg"
   domain_memory = 512

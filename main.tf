c34d2e7e136bf93717558cb58707e97679d91dac
krebsalad <johanmgina7@gmail.com>
2019-10-12 02:23:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
-module "module1" {
-  source = "./modules/ubuntu_1604_template_data/"
+# launch picalc servers
+module "picalc_service_module" {
+  source = "./modules/ibvirt_dual_picalc_server/"
 }
+
+# don't forget to change ip's to load balance for in config folder of the module
+module "loadbalancing_module" {
+  source = "./modules/ibvirt_load_balancer/"
+}
+  

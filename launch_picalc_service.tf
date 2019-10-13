5b6f84aa208f4f8bb004468fa2914f27d28acabb
krebsalad <johanmgina7@gmail.com>
2019-10-13 13:48:57 +0200

    Update launch_picalc_service.tf

--- launch_picalc_service.tf
+++ launch_picalc_service.tf
@@ -3,10 +3,10 @@ module "picalc_network_module" {
   source = "./modules/libvirt_picalc_network/"
 }
 
-# launch picalc servers
-# module "picalc_service_module" {
-#  source = "./modules/libvirt_dual_picalc_server/"
-#}
+launch picalc servers
+module "picalc_service_module" {
+  source = "./modules/libvirt_dual_picalc_server/"
+}
 
 # don't forget to change ip's to load balance for in config folder of the module
 module "loadbalancing_module" {
